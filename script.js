02071b340c326cbd1f6523976bc21ed446209ede
Elvinas <elviz.nejuz@gmail.com>
2022-03-13 10:33:06 +0200

    done the size functionality

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-const gridContainer = document.querySelector(".grid__container");
+let gridContainer = document.querySelector(".grid__container");
 const body = document.querySelector("html");
-console.log(body);
+const slider = document.querySelector(".size__slider");
+const sizeText = document.querySelector(".size__paragraph");
+slider.value = 16;
+
+sizeText.textContent = `${slider.value} X ${slider.value}`;
 let height = gridContainer.clientHeight;
-console.log(height);
-let size = 50;
-const rowHeight = height / size;
-const columnWidth = rowHeight;
+
+
 let selectedColor = "black"
-console.log(rowHeight);
-for(let i = 0; i < size; i++){
-    const row = document.createElement('div');
-    
-    row.classList.add("grid__row")
-    {
-        for(let i = 0; i < size; i++){
-            const divInner = document.createElement('div');
-            divInner.style.width = `${columnWidth}px`
-            divInner.classList.add("grid__element")
-            row.appendChild(divInner);
-        }
-    }
-    row.style.minHeight = `${rowHeight}px`;
-    gridContainer.appendChild(row);
-}
+generateGrid();
 
 gridContainer.addEventListener("mouseover", (e) => changeColor(e));
-/*
-items.forEach(item => {
-    item.addEventListener("mousedown", function(e){
-        e.target.style.backgroundColor = "blue";
-        items.forEach(item => { item.addEventListener("mouseover", function(e){
-            e.target.style.backgroundColor = "blue";
-        })})
-    })
-})*/
+slider.addEventListener("input", function(e) {
+    sizeText.textContent = `${slider.value} X ${slider.value}`;
+    size = slider.value;
+    generateGrid();
+})
 
 function changeColor(e){
-    if(e.buttons === 1){
-        console.log(e);
+    if(e.buttons > 0){
         e.target.style.backgroundColor = selectedColor;
     }
     else{
         return;
+    }
+}
+
+function generateGrid(){
+    gridContainer.textContent = "";
+    let size = slider.value;
+    let rowHeight = height / size;
+    let columnWidth = rowHeight;
+    for(let i = 0; i < size; i++){
+        const row = document.createElement('div');
+        
+        row.classList.add("grid__row")
+        {
+            for(let i = 0; i < size; i++){
+                const divInner = document.createElement('div');
+                divInner.style.width = `${columnWidth}px`
+                divInner.classList.add("grid__element")
+                row.appendChild(divInner);
+            }
+        }
+        row.style.minHeight = `${rowHeight}px`;
+        gridContainer.appendChild(row);
     }
 }
 
